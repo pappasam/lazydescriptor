0ba7957f1e51a881a7450c3730359ebb1fbecdfa
Sam Roeca <samuel.roeca@gmail.com>
2022-09-21 17:28:44 -0700

    Make work more generically

--- lazydescriptor/__init__.py
+++ lazydescriptor/__init__.py
@@ -1,52 +1,55 @@
 import inspect
-from typing import Callable, Generic, TypeVar
+from typing import Callable, Generic, TypeVar, Union, overload
 
-__all__ = ["Lazy", "lazy"]
+__all__ = ["Lazy", "LazyDesc", "lazy"]
 
 _SENTINEL = object()
 
 T = TypeVar("T")
 
 
-class Lazy(Generic[T]):
+class LazyDesc(Generic[T]):
     """A lazy data descriptor.
 
     Contains a few more things for mypy
     """
 
     def __init__(self) -> None:
-        self.value = _SENTINEL
-        self.called = False
+        self._value = _SENTINEL
+        self._called = False
+
+    def set_callable_value_manually(self, value: Callable[[], T]) -> None:
+        """Function to set the value manually to a callable."""
+        if not callable(value) or len(inspect.signature(value).parameters) > 0:
+            raise TypeError("Must be a callable with 0 parameters")
+        self._called = False
+        self._value = value
 
     def __set_name__(self, owner, name):
-        if self.value is not _SENTINEL:
+        if self._value is not _SENTINEL:
             raise TypeError("Do not set value when using as a descriptor")
 
     def __call__(self) -> T:
-        if (
-            not callable(self.value)
-            or len(inspect.signature(self.value).parameters) > 0
-        ):
-            raise TypeError("Must be a callable with 0 parameters")
-        return self.value()
+        return self._value()  # type: ignore
 
     def __get__(self, obj, objtype=None) -> T:
         if obj is None:
-            return self.value  # type: ignore
-        if not self.called:
-            self.value = self.value()  # type: ignore
-            self.called = True
-        return self.value  # type: ignore
+            return self._value  # type: ignore
+        if not self._called:
+            self._value = self._value()  # type: ignore
+            self._called = True
+        return self._value  # type: ignore
 
     def __set__(self, obj, value: "Lazy[T]") -> None:
-        if not callable(value) or len(inspect.signature(value).parameters) > 0:
-            raise TypeError("Must be a callable with 0 parameters")
-        self.called = False
-        self.value = value
+        self._called = not isinstance(value, LazyDesc)
+        self._value = value
 
 
-def lazy(value: Callable[[], T]) -> Lazy[T]:
+Lazy = Union[T, LazyDesc[T]]
+
+
+def lazy(value: Callable[[], T]) -> LazyDesc[T]:
     """Get a lazy thing, setting a value."""
-    instance: Lazy[T] = Lazy()
-    instance.value = value
+    instance: LazyDesc[T] = LazyDesc()
+    instance.set_callable_value_manually(value)
     return instance
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from dataclasses import dataclass
 
-from lazydescriptor import Lazy, lazy
+from lazydescriptor import Lazy, LazyDesc, lazy
 
 
 @dataclass
@@ -10,17 +10,28 @@ class Test:
     """Testing class for lazy values."""
 
     my_normal: int
-    my_int: Lazy[int] = Lazy()
-    my_str: Lazy[str] = Lazy()
-    my_list: Lazy[list[str]] = Lazy()
+    my_int: Lazy[int] = LazyDesc()
+    my_str: Lazy[str] = LazyDesc()
+    my_list: Lazy[list[str]] = LazyDesc()
 
 
 test = Test(
     my_normal=13,
     my_int=lazy(lambda: 12),
+    # my_int=12,
     my_str=lazy(lambda: "hello"),
     my_list=lazy(lambda: [str(i) for i in range(10)]),
 )
+
+
+def hello(x: int) -> int:
+    print("I ran!", x)
+    return x
+
+
+hello(test.my_int)
+
+test.my_int = 13
 
 for _ in range(3):
     print(test.my_int)
